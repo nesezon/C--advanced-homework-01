bdf0ab901e3e673ec266b132f4812fb778db4140
nesezon <tilichenko@gmail.com>
2021-04-08 23:40:46 +0300

    Задание 1. Library

---
+++ Library/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Libraty {
+  class Program {
+    static void Main(string[] args) {
+      var book1 = new Book<int>() { Name = "War and Peace", Price = 29 };
+      var book2 = new Book<double>() { Name = "Hamlet", Price = 9.99 };
+      book1.Show();
+      book2.Show();
+    }
+  }
+
+  class Book<T> {
+    private string name;
+    
+    private T price;
+    
+    public string Name {
+      get => name;
+      set => name = value;
+    }
+
+    public T Price {
+      get => price;
+      set => price = value;
+    }
+
+    public void Show() {
+      Console.WriteLine($"Name: {Name}");
+      Console.WriteLine($"Price {Price}");
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+C# Advanced. Homework 01
+## Generics
