6c9354fd42da3e0bb20486f6dde1f86bc62e2e24
nesezon <tilichenko@gmail.com>
2021-04-09 00:56:27 +0300

    Задание 2. Factory

---
+++ 2 Facroty/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace Facroty {
+  class Program {
+    static void Main(string[] args) {
+      var a = MyClass<int>.FacrotyMethod();
+      var b = MyClass<double>.FacrotyMethod();
+      var c = MyClass<string>.FacrotyMethod();
+      var d = MyClass<StringBuilder>.FacrotyMethod();
+      Console.WriteLine(a.GetType());
+      Console.WriteLine(b.GetType());
+      Console.WriteLine(c.GetType());
+      Console.WriteLine(d.GetType());
+    }
+  }
+
+  class MyClass<T> {
+    public static T FacrotyMethod() {
+      if (typeof(T) == typeof(string)) return (T) (object) string.Empty;
+      else return (T)Activator.CreateInstance(typeof(T));
+    }
+  }
+}
