f26de86ca89bbe5f024e9b5e0aecc4b359e38bc1
nesezon <tilichenko@gmail.com>
2021-04-09 09:54:24 +0300

    Задание 4. Nullable

---
+++ 4 Nullable/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Nullable {
+  class Program {
+    static void Main(string[] args) {
+      // Значимые типы по умолчанию не nullable
+      // DateTime time;
+
+      // Чтобы DateTime позволял хранить null нужно его уговорить (добавить вопросик)
+      DateTime? time = null;
+      if (time == null) {
+        /* do something */
+      }
+    }
+  }
+}
